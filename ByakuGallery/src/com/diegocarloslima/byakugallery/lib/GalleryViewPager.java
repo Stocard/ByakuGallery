7d586a0c3eb729fce36e3cb539bad15aaed86de0
Tobias Schürg <tobiasschuerg@gmail.com>
2017-08-23 13:35:50 +0200

    byaku also using ext.support_lib for version

--- ByakuGallery/src/com/diegocarloslima/byakugallery/lib/GalleryViewPager.java
+++ ByakuGallery/src/com/diegocarloslima/byakugallery/lib/GalleryViewPager.java
@@ -7,23 +7,23 @@ import android.util.AttributeSet;
 import android.view.View;
 
 public class GalleryViewPager extends ViewPager {
-	
-	
-	public GalleryViewPager(Context context) {
-		this(context, null);
-	}
 
-	public GalleryViewPager(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
-	
-	@SuppressLint("NewApi")
-	@Override
-	protected boolean canScroll(View v, boolean checkV, int dx, int x, int y) {
-		if (v instanceof TouchImageView) {
-			return ((TouchImageView) v).canScrollHorizontally(dx);
-		} else {
-			return super.canScroll(v, checkV, dx, x, y);
-		}
-	}
+
+    public GalleryViewPager(Context context) {
+        this(context, null);
+    }
+
+    public GalleryViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    protected boolean canScroll(View v, boolean checkV, int dx, int x, int y) {
+        if (v instanceof TouchImageView) {
+            return ((TouchImageView) v).canScrollHorizontally(dx);
+        } else {
+            return super.canScroll(v, checkV, dx, x, y);
+        }
+    }
 }
